c093f9b8e052f2bb1f90f00093240b541f355416
SDSmalin <37438286+SDSmalin@users.noreply.github.com>
2019-02-27 14:13:52 +0800

    Update OAuth.java
    添加注释

--- src/main/java/com/wechat/oauth/bean/OAuth.java
+++ src/main/java/com/wechat/oauth/bean/OAuth.java
@@ -5,18 +5,36 @@ package com.wechat.oauth.bean;
  * @author malin
  */
 public class OAuth {
-
+	/**
+	 * 公众号的唯一标识
+	 */
 	private String appid;
-
+	/**
+	 * 应用授权作用域，snsapi_base （不弹出授权页面，直接跳转，只能获取用户openid），
+	 * snsapi_userinfo （弹出授权页面，可通过openid拿到昵称、性别、所在地。并且， 即使在未关注的情况下，只要用户授权，
+	 * 也能获取其信息 ）
+	 */
 	private String scope;
-
+	/**
+	 * 重定向后会带上state参数，开发者可以填写a-zA-Z0-9的参数值，最多128字节
+	 */
 	private String state;
-
+	/**
+	 * 用于接收微信授权成功后返回的code值
+	 */
 	private String code;
-
+	/**
+	 * 授权后重定向的回调链接地址， 请使用 urlEncode 对链接进行处理
+	 */
 	private String redirect_uri;
-
+	/**
+	 * 协议
+	 */
 	private String protocol;
+	/**
+	 * 返回类型，请填写code
+	 */
+	private String response_type;
 
 	public OAuth(){
 		appid = "";
@@ -69,4 +87,11 @@ public class OAuth {
 		this.protocol = protocol;
 	}
 
+	public String getResponse_type() {
+		return response_type;
+	}
+
+	public void setResponse_type(String response_type) {
+		this.response_type = response_type;
+	}
 }
